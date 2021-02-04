8c3dbb5462936286e5bcf23d6f9033f671790114
CaitlynP123 <74350761+CaitlynP123@users.noreply.github.com>
2021-02-04 13:13:37 +0530

    Update Umbrella.js

--- Umbrella.js
+++ Umbrella.js
@@ -13,14 +13,14 @@ class Umbrella{
         "proc41images/images/Walking Frame/walking_2.png","proc41images/images/Walking Frame/walking_3.png",
         "proc41images/images/Walking Frame/walking_4.png","proc41images/images/Walking Frame/walking_5.png",
         "proc41images/images/Walking Frame/walking_6.png","proc41images/images/Walking Frame/walking_7.png")
+        
+        var boy = createSprite(400,500)
+        boy.addAnimation("walking", this.boyImg)
+        boy.scale = 0.5
 }
 
     display(){
         var pos = this.umbrella.position
-
-        var boy = createSprite(400,500)
-        boy.addAnimation("walking", this.boyImg)
-        boy.scale = 0.5
 
         fill("black")
         ellipseMode(CENTER)
